6d5bbc4a362d302aefeba0c9b46a930d7c0a9aa4
Lautaro Guardia <lautiguardia12@gmail.com>
2026-03-11 21:12:23 -0300

    Mi primer commit

---
+++ repaso.py
@@ -0,0 +1,16 @@
+#comentario en linea      
+"""comentario en bloque
+y eso """
+
+print("calculadora pyhton ")
+
+#num1 = 4
+#num2 = 6
+
+numero1 = int(input("ingrese un numero : "))
+
+numero2 = int(input("ingrese un numero : "))
+
+suma = numero1 + numero2
+
+print(f"el resultado de la suma es : {suma}")
